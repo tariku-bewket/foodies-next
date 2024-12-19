5fd585e021e802fbd07304b5a6eca30ecb018e09
Tariku Bewket <tarikubew@gmail.com>
2024-12-19 13:18:35 +0300

    add meals page contents and meal grid component

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,15 +1,26 @@
 import Link from 'next/link';
 
+import classes from './page.module.css';
+import MealsGrid from '@/components/meals/meals-grid';
+
 export default function MealsPage() {
   return (
-    <main>
-      <h1>All Meals</h1>
-      <p>
-        <Link href='/meals/meal-1'>Meal 1</Link>
-      </p>
-      <p>
-        <Link href='/meals/meal-2'>Meal 2</Link>
-      </p>
-    </main>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{' '}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Chooose your favorite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href='/meals/share'>Share Your Favourite Meals</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
   );
 }
