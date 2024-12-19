add6ad1e0f20abf47d08711d5ab3c7ecbcc208e9
Tariku Bewket <tarikubew@gmail.com>
2024-12-19 17:23:38 +0300

    add server actions to submit form data to the server

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,5 +1,6 @@
 import ImagePicker from '@/components/meals/image-picker';
 import classes from './page.module.css';
+import { shareMeals } from '@/lib/actions';
 
 export default function ShareMealPage() {
   return (
@@ -11,7 +12,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form}>
+        <form className={classes.form} action={shareMeals}>
           <div className={classes.row}>
             <p>
               <label htmlFor='name'>Your name</label>
