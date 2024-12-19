d10674e6af2931eb26805a505153bfd8312d00c2
Tariku Bewket <tarikubew@gmail.com>
2024-12-19 16:44:41 +0300

    add image preview logic and structure

--- components/meals/image-picker.js
+++ components/meals/image-picker.js
@@ -1,19 +1,50 @@
 'use client';
 
-import { useRef } from 'react';
+import Image from 'next/image';
+
+import { useRef, useState } from 'react';
 import classes from './image-picker.module.css';
 
 export default function ImagePicker({ label, name }) {
+  const [pickedImagePath, setPickedImagePath] = useState();
   const imageInput = useRef();
 
   function handlePickImage() {
-    inputRef.current.click();
+    imageInput.current.click();
+  }
+
+  function hadleImageChange(event) {
+    const file = event.target.files[0];
+
+    if (!file) {
+      return;
+    }
+
+    const fileReader = new FileReader();
+
+    fileReader.onload = () => {
+      setPickedImagePath(fileReader.result);
+    };
+
+    fileReader.readAsDataURL(file);
   }
 
   return (
     <div className={classes.picker}>
       <label htmlFor={name}>{label}</label>
+
       <div className={classes.controls}>
+        <div className={classes.preview}>
+          {!pickedImagePath && <p>No Image Picked</p>}
+          {pickedImagePath && (
+            <Image
+              src={pickedImagePath}
+              alt='A preview of the picked image'
+              fill
+            />
+          )}
+        </div>
+
         <input
           className={classes.input}
           type='file'
@@ -21,13 +52,14 @@ export default function ImagePicker({ label, name }) {
           id={name}
           name={name}
           ref={imageInput}
+          onChange={hadleImageChange}
         />
         <button
           className={classes.button}
           type='button'
           onClick={handlePickImage}
         >
-          Upload
+          Pick an Image
         </button>
       </div>
     </div>
