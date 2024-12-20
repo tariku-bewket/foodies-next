b4ee3bb5689262fa37a65fc7ff24f35b66033999
Tariku Bewket <tarikubew@gmail.com>
2024-12-20 14:56:27 +0300

    add dynamic metadata to dynamic pages

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -4,6 +4,19 @@ import { notFound } from 'next/navigation';
 import classes from './page.module.css';
 import { getMeal } from '@/lib/meals';
 
+export async function generateMetadata({ params }) {
+  const meal = await getMeal(params.mealSlug);
+
+  if (!meal) {
+    notFound();
+  }
+
+  return {
+    title: meal.title,
+    description: meal.summary,
+  };
+}
+
 export default async function MealDetailsPage({ params }) {
   const meal = await getMeal(params.mealSlug);
 
