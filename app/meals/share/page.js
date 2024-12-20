1ff9b0a630b4516e49a2ce75cd5fdff99e0b94b6
Tariku Bewket <tarikubew@gmail.com>
2024-12-20 11:52:16 +0300

    validate user inputs from server and show messages to the client

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,7 +1,5 @@
-import ImagePicker from '@/components/meals/image-picker';
-import classes from './page.module.css';
-import { shareMeals } from '@/lib/actions';
 import MealsFormSubmit from '@/components/meals/meals-form-submit';
+import classes from './page.module.css';
 
 export default function ShareMealPage() {
   return (
@@ -13,39 +11,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form} action={shareMeals}>
-          <div className={classes.row}>
-            <p>
-              <label htmlFor='name'>Your name</label>
-              <input type='text' id='name' name='name' required />
-            </p>
-            <p>
-              <label htmlFor='email'>Your email</label>
-              <input type='email' id='email' name='email' required />
-            </p>
-          </div>
-          <p>
-            <label htmlFor='title'>Title</label>
-            <input type='text' id='title' name='title' required />
-          </p>
-          <p>
-            <label htmlFor='summary'>Short Summary</label>
-            <input type='text' id='summary' name='summary' required />
-          </p>
-          <p>
-            <label htmlFor='instructions'>Instructions</label>
-            <textarea
-              id='instructions'
-              name='instructions'
-              rows='10'
-              required
-            ></textarea>
-          </p>
-          <ImagePicker label='Pick an image' name='image' />
-          <p className={classes.actions}>
-            <MealsFormSubmit />
-          </p>
-        </form>
+        <MealsFormSubmit />
       </main>
     </>
   );
--- components/meals/meals-form-submit.js
+++ components/meals/meals-form-submit.js
@@ -1,13 +1,54 @@
 'use client';
 
-import { useFormStatus } from 'react-dom';
+import { useFormState, useFormStatus } from 'react-dom';
+
+import { shareMeals } from '@/lib/actions';
+import ImagePicker from './image-picker';
+import classes from './meals-form-submit.module.css';
 
 export default function MealsFormSubmit() {
   const { pending } = useFormStatus();
+  const [state, formAction] = useFormState(shareMeals, { message: null });
 
   return (
-    <button type='submit' disabled={pending}>
-      {pending ? 'Submitting...' : 'Share Meal'}
-    </button>
+    <form className={classes.form} action={formAction}>
+      <div className={classes.row}>
+        <p>
+          <label htmlFor='name'>Your name</label>
+          <input type='text' id='name' name='name' required />
+        </p>
+        <p>
+          <label htmlFor='email'>Your email</label>
+          <input type='email' id='email' name='email' required />
+        </p>
+      </div>
+
+      <p>
+        <label htmlFor='title'>Title</label>
+        <input type='text' id='title' name='title' required />
+      </p>
+      <p>
+        <label htmlFor='summary'>Short Summary</label>
+        <input type='text' id='summary' name='summary' required />
+      </p>
+      <p>
+        <label htmlFor='instructions'>Instructions</label>
+        <textarea
+          id='instructions'
+          name='instructions'
+          rows='10'
+          required
+        ></textarea>
+      </p>
+
+      <ImagePicker label='Pick an image' name='image' />
+      {state.message && <p>{state.message}</p>}
+
+      <p className={classes.actions}>
+        <button type='submit' disabled={pending}>
+          {pending ? 'Submitting...' : 'Share Meal'}
+        </button>
+      </p>
+    </form>
   );
 }
